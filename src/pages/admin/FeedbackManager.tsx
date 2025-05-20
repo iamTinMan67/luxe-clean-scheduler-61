38767087a23a53d5b54ff72dc304b4b8055a3138
iamTinMan67 <tomboyce01@gmail.com>
2025-05-20 23:45:14 +0100

    manage

--- src/pages/admin/FeedbackManager.tsx
+++ src/pages/admin/FeedbackManager.tsx
@@ -26,7 +26,7 @@ const FeedbackManager = () => {
 
   return (
     <div className="container px-4 py-8">
-      <AdminPageTitle title="Feedback Manager" />
+      <AdminPageTitle title="Manage" />
       
       <FeedbackTabs 
         feedback={feedback} 
